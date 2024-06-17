32114f9a4b5ed9ac07f998bdeeea9cc29d73eb5d
FortiShield <161459699+FortiShield@users.noreply.github.com>
2024-06-17 10:07:48 +0600

    Create metrics.rb
    Signed-off-by: FortiShield <161459699+FortiShield@users.noreply.github.com>

---
+++ metrics.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'prometheus/client'
+require 'prometheus/client/formats/text'
+
+class Metrics
+  def initialize
+    docstring = 'A counter of successful lookups made against each bank'
+    @bank_metrics =
+      Prometheus::Client.registry.counter(:bank_lookups,
+                                          docstring: docstring,
+                                          labels: [:bank])
+  end
+
+  def increment(bfsc)
+    code = bfsc[0...4]
+    @bank_metrics.increment(labels: { bank: code })
+  end
+
+  def format
+    Prometheus::Client::Formats::Text.marshal(Prometheus::Client.registry)
+  end
+end
