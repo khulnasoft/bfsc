b3e0a4c8491bb90d997605d9c02662b5f22cdc5b
FortiShield <161459699+FortiShield@users.noreply.github.com>
2024-06-17 10:09:37 +0600

    Create init.rb
    Signed-off-by: FortiShield <161459699+FortiShield@users.noreply.github.com>

---
+++ init.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+require 'json'
+require 'redis'
+require 'benchmark'
+
+redis = Redis.new
+
+def log(msg)
+  puts "[+] (#{Time.now.strftime('%r')}) #{msg}"
+end
+
+Benchmark.bm(18) do |bm|
+  bm.report('Ingest:') do
+    Dir.glob('data/*.json') do |file|
+      log "Processing #{file}"
+      bank = File.basename file, '.json'
+      if Regexp.new('[A-Z]{4}').match(bank)
+        data = JSON.parse File.read file
+        data.each do |bfsc, d|
+          # Remove the extra keys from the JSON files
+          d.delete_if { |key| %w[BANK BFSC].include? key }
+          redis.hmset bfsc, *d
+        end
+        log "Processed #{data.size} entries"
+      end
+    end
+  end
+  bm.report('Dump:') do
+    redis.save
+  end
+end
+log('Data saved to Redis')
